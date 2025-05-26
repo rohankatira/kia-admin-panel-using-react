76905ad16e2c1302324adc55b5059b6c77973314
rohankatira <rohankatira1@gmail.com>
2025-05-26 18:18:12 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Slider from '../components/Slider'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <>
+      <Slider />
+      <Header/>
+    </>
+  )
+}
+
+export default Home
