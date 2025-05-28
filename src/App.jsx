b1fb921f7f84510fd1d14606eb33a15039075693
rohankatira <rohankatira1@gmail.com>
2025-05-28 23:14:48 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import'./assets/css/bootstrap.min.css'
+import'./assets/css/fonts.min.css'
+import'./assets/css/kaiadmin.min.css'
+import'./assets/css/plugins.min.css'
 import Home from './pages/Home'
-
+import Form from './pages/Form'
 const App = () => {
   return (
     <>
